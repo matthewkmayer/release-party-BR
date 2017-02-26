cfb037234aa7ed2d5998f1dd694186f92dbc166f
Matthew Mayer <matthewkmayer@gmail.com>
2017-02-26 13:57:11 -0800

    Refactor: pulls behavior into github module.

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -1,4 +1,8 @@
+extern crate reqwest;
 extern crate serde_json;
+
+use std::io::Read;
+
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GithubRepo {
@@ -7,7 +11,19 @@ pub struct GithubRepo {
     pub url: String
 }
 
-pub fn repo_list_from_string(json_str: String) -> Result<Vec<GithubRepo>, String> {
+pub fn get_repos_at(repos_url: &str) -> Result<Vec<GithubRepo>, String> {
+    let mut resp = reqwest::get(repos_url).unwrap();
+    let mut buffer = String::new();
+
+    match resp.read_to_string(&mut buffer) {
+        Ok(_) => (),
+        Err(e) => println!("error reading response: {}", e),
+    }
+
+    return repo_list_from_string(buffer);
+}
+
+fn repo_list_from_string(json_str: String) -> Result<Vec<GithubRepo>, String> {
     // This looks a bit weird due to supplying type hints to deserialize:
     let _ : Vec<GithubRepo> = match serde_json::from_str(&json_str)  {
         Ok(v) => return Ok(v),
@@ -15,6 +31,10 @@ pub fn repo_list_from_string(json_str: String) -> Result<Vec<GithubRepo>, String
     };
 }
 
-pub fn release_needed(repo: &GithubRepo) -> bool {
-    false
+// Try to create the release PR and return the URL of it:
+pub fn create_release_pull_request(repo: &GithubRepo) -> Result<String, String> {
+    if repo.name == "calagator" {
+        return Err("already up to date".to_string());
+    }
+    Ok("https://github.whatever/reponame/prs/1".to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate reqwest;
 
 use std::env;
 use std::env::VarError;
-use std::io::Read;
 
 mod github;
 
@@ -15,18 +13,15 @@ fn main() {
     let token = get_github_token().unwrap();
     println!("Would use this github token: {}", token);
 
-    let mut resp = reqwest::get("https://api.github.com/users/matthewkmayer/repos").unwrap();
-    let mut buffer = String::new();
+    // get list of repos from location foo:
+    let repos = github::get_repos_at("https://api.github.com/users/matthewkmayer/repos").unwrap();
 
-    match resp.read_to_string(&mut buffer) {
-        Ok(_) => (),
-        Err(e) => println!("error reading response: {}", e),
-    }
-
-    let list_of_repos = github::repo_list_from_string(buffer).unwrap();
-
-    for repo in &list_of_repos {
-        println!("Does {} need a release? {}", repo.name, github::release_needed(repo));
+    // for each repo, attempt to create release PR:
+    for repo in &repos {
+        match github::create_release_pull_request(repo) {
+            Ok(pr_url) => println!("Made PR for {}: {}", repo.name, pr_url),
+            Err(e) => println!("Couldn't create PR for {}: {}", repo.name, e),
+        }
     }
 }
 
