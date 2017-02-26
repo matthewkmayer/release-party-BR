559861905b8e022c38502d403ce50d6d958d6412
Matthew Mayer <matthewkmayer@gmail.com>
2017-02-26 12:52:21 -0800

    Starting to talk to github api.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # release-party-BR
 Release party automation
+
+### Game plan
+1. Be able to talk to github API.
+2. Get list of repos from an org.
+3. Check each repo to see if `release` branch is behind `master`.
+4. If a repo is, create new PR from `master` to `release`.
+5. Show a list of all repos and their status: up to date or which PR brings `release` up to speed.
+
+### Future work
+1. Make which org configurable.
+2. Accept list of repos to ignore.
+3. Links to PRs for releasing.
+4. Put everything behind a RESTful API to allow fancy web interface.
+5. Talk to CI to get status of PR?
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate reqwest;
+
+use std::env;
+use std::io::Read;
+
 fn main() {
     println!("Hello, world!");
+    let key = "RP_GITHUBTOKEN";
+    match env::var(key) {
+        Ok(val) => println!("{}: {:?}", key, val),
+        Err(e) => println!("Couldn't find {}: {}", key, e),
+    }
+
+    let mut resp = reqwest::get("https://api.github.com/users/matthewkmayer/repos").unwrap();
+    println!("resp is: {:?}", resp);
+    let mut buffer = String::new();
+
+    match resp.read_to_string(&mut buffer) {
+        Ok(_) => (),
+        Err(e) => println!("error reading response: {}", e),
+    }
+    println!("request body: {}", buffer);
 }
