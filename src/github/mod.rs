280dd046f62ea2049cd8869c7c64c71338ab7554
Matthew Mayer <matthewkmayer@gmail.com>
2017-02-26 13:40:41 -0800

    Adds skeleton for seeing if repo needs a release PR.

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -3,16 +3,18 @@ extern crate serde_json;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GithubRepo {
     id: i32,
-    name: String,
-    url: String
+    pub name: String,
+    pub url: String
 }
 
 pub fn repo_list_from_string(json_str: String) -> Result<Vec<GithubRepo>, String> {
-    let repo_list : Vec<GithubRepo> = match serde_json::from_str(&json_str)  {
-        Ok(v) => v,
+    // This looks a bit weird due to supplying type hints to deserialize:
+    let _ : Vec<GithubRepo> = match serde_json::from_str(&json_str)  {
+        Ok(v) => return Ok(v),
         Err(e) => return Err(format!("Couldn't deserialize it: {}", e)),
     };
+}
 
-    println!("repo_list is {:?}", repo_list);
-    return Ok(repo_list);
+pub fn release_needed(repo: &GithubRepo) -> bool {
+    false
 }
--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,31 @@ extern crate serde_derive;
 extern crate reqwest;
 
 use std::env;
+use std::env::VarError;
 use std::io::Read;
 
 mod github;
 
 fn main() {
-    println!("Hello, world!");
-    let key = "RP_GITHUBTOKEN";
-    match env::var(key) {
-        Ok(val) => println!("{}: {:?}", key, val),
-        Err(e) => println!("Couldn't find {}: {}", key, e),
-    }
+    let token = get_github_token().unwrap();
+    println!("Would use this github token: {}", token);
 
     let mut resp = reqwest::get("https://api.github.com/users/matthewkmayer/repos").unwrap();
-    // println!("resp is: {:?}", resp);
     let mut buffer = String::new();
 
     match resp.read_to_string(&mut buffer) {
         Ok(_) => (),
         Err(e) => println!("error reading response: {}", e),
     }
-    // println!("request body: {}", buffer);
 
-    let foo = github::repo_list_from_string(buffer).unwrap();
+    let list_of_repos = github::repo_list_from_string(buffer).unwrap();
 
-    
+    for repo in &list_of_repos {
+        println!("Does {} need a release? {}", repo.name, github::release_needed(repo));
+    }
+}
+
+fn get_github_token() -> Result<String, VarError> {
+    let key = "RP_GITHUBTOKEN";
+    env::var(key)
 }
